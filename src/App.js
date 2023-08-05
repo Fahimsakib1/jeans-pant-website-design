f051e6ce2b90864bc64746cd515cb730bf965faf
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2023-08-06 05:20:31 +0600

    upload website design to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import FifthPage from './components/FifthPage/FifthPage';
+import Footer from './components/Footer/Footer';
+import ForthPage from './components/ForthPage/ForthPage';
+import LandingPage from './components/LandingPage/LandingPage';
+import SecondPage from './components/SecondPage/SecondPage';
+import SixthPage from './components/SixthPage/SixthPage';
+import ThirdPage from './components/ThirdPage/ThirdPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LandingPage></LandingPage>
+      <SecondPage></SecondPage>
+      <ThirdPage></ThirdPage>
+      <ForthPage></ForthPage>
+      <FifthPage></FifthPage>
+      <SixthPage></SixthPage>
+      <Footer></Footer>
     </div>
   );
 }
